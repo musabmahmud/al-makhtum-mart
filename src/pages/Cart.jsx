60f012dfe55fe0261cec8df01951d9fd5eba8965
musabmahmud <md.musabmahmud100@gmail.com>
2024-08-30 12:47:49 +0600

    place-order & order

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -3,6 +3,7 @@ import { ShopContext } from '../context/ShopContext'
 import Title from '../components/Title';
 import { assets } from '../assets/assets';
 import CartTotal from '../components/CartTotal';
+import { Link } from 'react-router-dom';
 
 const Cart = () => {
 
@@ -64,8 +65,8 @@ const Cart = () => {
             <div className="flex justify-end my-20">
                 <div className="w-full sm:w-[450px]">
                     <CartTotal />
-                    <div className="w-full text-end">
-                        <button className='bg-black text-white text-sm my-8 px-8 py-3 rounded'>PROCEED TO CHECKOUT</button>
+                    <div className="w-full text-end mt-8">
+                        <Link to="/place-order" className='bg-black text-white text-sm my-8 px-8 py-3 rounded'>PROCEED TO CHECKOUT</Link>
                     </div>
                 </div>
             </div>
